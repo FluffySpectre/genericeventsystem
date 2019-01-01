947daa8684740921cb013d43d5c246045a3d4c87
Björn Bosse <b.bosse1991@gmail.com>
2019-01-01 14:55:40 +0100

    Renamed namespace to match the components name

--- src/EventSystem.cs
+++ src/EventSystem.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace BetterEventSystem
+namespace GenericEventSystem
 {
 	public abstract class EventSystem {
 		public abstract void CleanEventSystem();
